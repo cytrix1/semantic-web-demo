67bd9bd462dce5c828171dee7f653949da0ecc3d
cytrix1 <bioinfosong@gmail.com>
2012-04-29 01:50:06 -0500

    show img changed

--- 2imgload.js
+++ 2imgload.js
@@ -52,38 +52,72 @@ show_img = function(r, i, j) {
   row = t.insertRow(2);
   cell1 = row.insertCell(0);
   cell2 = row.insertCell(1);
-  bt1 = document.createElement('button');
-  bt2 = document.createElement('button');
-  bt3 = document.createElement('button');
-  bt4 = document.createElement('button');
-  bt1.innerHTML = "&lt;&lt;";
-  bt2.innerHTML = "&gt;&gt";
-  bt3.innerHTML = "&lt;&lt;";
-  bt4.innerHTML = "&gt;&gt";
-  cell1.appendChild(bt1);
-  cell1.appendChild(bt2);
-  cell2.appendChild(bt3);
-  cell2.appendChild(bt4);
-  bt1.addEventListener("click", function (ev) {
-      ev.preventDefault();
-      document.body.removeChild(t);
-      show_img(r, --i, j);
-  });
-  bt2.addEventListener("click", function (ev) {
-      ev.preventDefault();
-      document.body.removeChild(t); 
-      show_img(r, ++i, j);
-  });
-  bt3.addEventListener("click", function (ev) {
-      ev.preventDefault();
-      document.body.removeChild(t);
-      show_img(r, i, --j);
-  });
-  bt4.addEventListener("click", function (ev) {
-      ev.preventDefault();
-      document.body.removeChild(t);
-      show_img(r, i, ++j);
-  });
+  var bt = new Array();
+  var ind = new Array();
+  if (i == 1) {
+      ind.push(2);   } 
+  else if (i > 1 && i < r.getElementsByName('url1').length) {
+      ind.push(1); ind.push(2);    }
+  else if (i == r.getElementsByName('url1').length ) {
+      ind.push(1);  }
+  else {
+      alert("abnormal value of i");    }
+  if (j == 1) {
+      ind.push(4);   } 
+  else if (j > 1 && j < r.getElementsByName('url2').length) {
+      ind.push(3); ind.push(4);    }
+  else if (j == r.getElementsByName('url2').length ) {
+      ind.push(3);  }
+  else {
+      alert("abnormal value of j");    }
+  for (each in ind)  {
+      bt[each] = document.createElement('button');
+      bt[each].innerHtml = (each % 2) ? "&lt;&lt;" : "&gt;&gt;";
+      if (each == 1 || each == 2)  {
+          cell1.appendChild(bt[each]);  }
+      else if (each == 3 || each ==4)  {
+          cell2.appendChild(bt[each]);  }
+      i = (each == 1) ? --i : ((each == 2) ? ++i : i);
+      j = (each == 3) ? --j : ((each == 4) ? ++j : j);
+      bt[each].addEventListener("click", function (ev) {
+          ev.preventDefault();
+          document.body.removeChild(t);
+          show_img(r, i, j);
+      });
+  
+
+  //bt1 = document.createElement('button');
+  //bt2 = document.createElement('button');
+  //bt3 = document.createElement('button');
+  //bt4 = document.createElement('button');
+  //bt1.innerHTML = "&lt;&lt;";
+  //bt2.innerHTML = "&gt;&gt;";
+  //bt3.innerHTML = "&lt;&lt;";
+  //bt4.innerHTML = "&gt;&gt;";
+  //cell1.appendChild(bt1);
+  //cell1.appendChild(bt2);
+  //cell2.appendChild(bt3);
+  //cell2.appendChild(bt4);
+  //bt1.addEventListener("click", function (ev) {
+  //    ev.preventDefault();
+  //    document.body.removeChild(t);
+  //    show_img(r, --i, j);
+  //});
+  //bt2.addEventListener("click", function (ev) {
+  //    ev.preventDefault();
+  //    document.body.removeChild(t); 
+  //    show_img(r, ++i, j);
+  //});
+  //bt3.addEventListener("click", function (ev) {
+  //    ev.preventDefault();
+  //    document.body.removeChild(t);
+  //    show_img(r, i, --j);
+  //});
+  //bt4.addEventListener("click", function (ev) {
+  //    ev.preventDefault();
+  //    document.body.removeChild(t);
+  //    show_img(r, i, ++j);
+  //});
   
 
 
